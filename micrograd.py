b387fc76060fc4a3c724ed6a4b0246d7b8f2a883
Dhakhxayah <dhachu09092005@gmail.com>
2025-05-07 15:04:48 +0530

    Appended graph codes

--- micrograd.py
+++ micrograd.py
@@ -1,8 +1,9 @@
-
+from graphviz import Digraph
 class Value:
     def __init__(self,data,_children=(),_op="", label = ""):
         self.data = data
         self.grad = 0.0
+        self.backward = lambda:None
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -17,7 +18,38 @@ class Value:
     def __mul__(self,other):
         out = Value(self.data * other.data , (self,other), "*")
         return out
+
+def trace(root):
+    nodes , edges = set() , set()
+    def build(v):
+        if v not in nodes:
+            nodes.add(v)
+            for child in v._prev:
+                edges.add((child,v))
+                build(child)
+    build(root)
+    return nodes, edges
+
+def draw_dot(root, format='svg', rankdir='LR'):
+    """
+    format: png | svg | ...
+    rankdir: TB (top to bottom graph) | LR (left to right)
+    """
+    assert rankdir in ['LR', 'TB']
+    nodes, edges = trace(root)
+    dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
     
+    for n in nodes:
+        dot.node(name=str(id(n)), label = "{ data %.4f | grad %.4f }" % (n.data, n.grad), shape='record')
+        if n._op:
+            dot.node(name=str(id(n)) + n._op, label=n._op)
+            dot.edge(str(id(n)) + n._op, str(id(n)))
+    
+    for n1, n2 in edges:
+        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
+    
+    return dot
+
 
 a = Value(2.0 , label = 'a')
 b = Value(-3.0, label = 'b')
