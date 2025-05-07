ffd27cc50350881ab1c1cd4ce290db40ff6b447f
Dhakhxayah <dhachu09092005@gmail.com>
2025-05-07 16:23:52 +0530

    Implemented graph

--- micrograd.py
+++ micrograd.py
@@ -1,9 +1,10 @@
-from graphviz import Digraph
+from graph import *
+
 class Value:
     def __init__(self,data,_children=(),_op="", label = ""):
         self.data = data
         self.grad = 0.0
-        self.backward = lambda:None
+        self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
         self.label = label
@@ -12,45 +13,86 @@ class Value:
         return f"Value(data = {self.data} , label = {self.label})"
     
     def __add__(self,other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data , (self,other) , "+")
+        
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+
         return out
     
     def __mul__(self,other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data , (self,other), "*")
+        
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        
         return out
 
-def trace(root):
-    nodes , edges = set() , set()
-    def build(v):
-        if v not in nodes:
-            nodes.add(v)
-            for child in v._prev:
-                edges.add((child,v))
-                build(child)
-    build(root)
-    return nodes, edges
+    def __pow__(self,other):
+        assert isinstance(other,(int,float))
+        out = Value(self.data**other , (self,),f'**{other}')
 
-def draw_dot(root, format='svg', rankdir='LR'):
-    """
-    format: png | svg | ...
-    rankdir: TB (top to bottom graph) | LR (left to right)
-    """
-    assert rankdir in ['LR', 'TB']
-    nodes, edges = trace(root)
-    dot = Digraph(format=format, graph_attr={'rankdir': rankdir}) #, node_attr={'rankdir': 'TB'})
-    
-    for n in nodes:
-        dot.node(name=str(id(n)), label = "{ data %.4f | grad %.4f }" % (n.data, n.grad), shape='record')
-        if n._op:
-            dot.node(name=str(id(n)) + n._op, label=n._op)
-            dot.edge(str(id(n)) + n._op, str(id(n)))
-    
-    for n1, n2 in edges:
-        dot.edge(str(id(n1)), str(id(n2)) + n2._op)
-    
-    return dot
+        def _backward():
+            self.grad += (other * self.data**(other-1)) * out.grad
+        out._backward = _backward
 
+        return out
+    
+    def relu(self):
+        out = Value(0 if self.data < 0 else self.data, (self,),'ReLU')
 
+        def _backward():
+            self.grad += (out.data > 0)* out.grad
+        out._backward = _backward
+
+        return out
+    
+    def backward(self):
+        topo = []
+        visited = set()
+        def build_topo(v):
+            if v not in visited:
+                visited.add(v)
+                for child in v._prev:
+                    build_topo(child)
+                topo.append(v)
+        build_topo(self)
+
+        self.grad = 1
+        for v in reversed(topo):
+            v._backward()
+
+    def __neg__(self):
+        return self * -1
+    
+    def __radd__(self,other):
+        return self + other
+    
+    def __sub__(self,other):
+        return self + (-other)
+    
+    def __rsub__(self,other):
+        return other + (-self)
+    
+    def __rmul__(self,other):
+        return self * other
+    
+    def __truediv__(self,other):
+        return self * other**-1
+    
+    def __rtruediv__(self,other):
+        return other * self**-1
+    
+    def __repr__(self):
+        return f"Value(data={self.data},grad={self.grad})"
+    
+        
 a = Value(2.0 , label = 'a')
 b = Value(-3.0, label = 'b')
 c = Value(10.0, label = 'c')
@@ -60,4 +102,7 @@ f = Value(-2.0,label='f')
 L = d * f ; L.label = 'L'
 
 print("ans=",L)
+L.backward()
 
+dot = draw_dot(L)
+dot.render('graph_output', format='png', view=True)
